777fb20a9dcacb3a3c34ce24c03de6604e0f31de
link28rus <link28rus@gmail.com>
2026-04-20 12:30:36 +1000

    feat(mobile-child): connectivity listener triggers immediate flush

--- apps/mobile-child/lib/features/home/home_controller.dart
+++ apps/mobile-child/lib/features/home/home_controller.dart
@@ -1,3 +1,4 @@
+import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import '../../core/native/location_service_channel.dart';
 import '../../data/database.dart';
@@ -23,4 +24,11 @@ final homeInitProvider = FutureProvider<void>((ref) async {
   final ch = ref.watch(serviceChannelProvider);
   ch.onLocation(ingestor.onLocation);
   await ch.startService();
+
+  final conn = Connectivity();
+  conn.onConnectivityChanged.listen((list) {
+    if (list.any((r) => r != ConnectivityResult.none)) {
+      ingestor.flushQueue();
+    }
+  });
 });
