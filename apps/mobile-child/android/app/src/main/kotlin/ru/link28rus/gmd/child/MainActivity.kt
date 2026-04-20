64d159409fdc69c80183b9e7241f3253e307eef6
link28rus <link28rus@gmail.com>
2026-04-20 12:21:47 +1000

    feat(mobile-child): Kotlin LocationForegroundService (Fused Provider)

--- apps/mobile-child/android/app/src/main/kotlin/ru/link28rus/gmd/child/MainActivity.kt
+++ apps/mobile-child/android/app/src/main/kotlin/ru/link28rus/gmd/child/MainActivity.kt
@@ -1,5 +1,12 @@
 package ru.link28rus.gmd.child
 
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.embedding.engine.FlutterEngineCache
 
-class MainActivity : FlutterActivity()
+class MainActivity : FlutterActivity() {
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+        FlutterEngineCache.getInstance().put(LocationForegroundService.ENGINE_ID, flutterEngine)
+    }
+}
